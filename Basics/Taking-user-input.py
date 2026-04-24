5486706ba62118a4ccc6543c3f49b4abb0ab9b5c
Atharv-prx <atharvvrm6969@gmail.com>
2026-04-24 20:43:52 +0530

    Remember to convert the default input into desired data type

---
+++ Basics/Taking-user-input.py
@@ -0,0 +1,7 @@
+#When we accept input it is always string data type by default unless we manually change
+
+name = input("Enter your name: ")
+print(f"Hello {name}")
+
+age = int(input ("What's your age"))
+print(f"You are {age} years old")
