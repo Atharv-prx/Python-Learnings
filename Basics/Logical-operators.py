330860c866b63eaae9d688d85bb32782082a75b2
Atharv-prx <atharvvrm6969@gmail.com>
2026-04-25 12:10:01 +0530

    Similar to c++

---
+++ Basics/Logical-operators.py
@@ -0,0 +1,7 @@
+#and,or,not
+temp = float(input("What's the temperature in degree celsius: "))
+
+if temp >= 0 and temp <= 30 :
+    print ("The Temp is pretty good")
+else:
+    print ("Temp is bad")
