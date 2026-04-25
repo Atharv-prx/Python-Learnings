23454297fb200a8c84d2a8174ecfe853265f47e1
Atharv-prx <atharvvrm6969@gmail.com>
2026-04-25 11:10:30 +0530

    Well these are some usefull maths function

---
+++ Maths/Basic-maths-function.py
@@ -0,0 +1,19 @@
+# These do not require math module
+# Let's say --> x = 3.14
+#               y = -4
+#               z = 5
+# result = round(x)  --> 3
+# result = abs(y)    --> 4
+# result = pow(2,3)  --> 8
+# result = max(x,y,z)--> 5
+# result = min(x,y,z)--> -4
+
+import math
+
+x=9.1
+
+print(f"Value of pie is {math.pi}")
+print(f"Value of e is {math.e}")
+print(f"Square root of 9 is {math.sqrt(9)}")
+print(f"Ceiling of x is {math.ceil(x)}")
+print(f"Floor of x is {math.floor(x)}")
