8295746bc4ba08d2be201dc5d9c3c9319fd709e4
Atharv-prx <atharvvrm6969@gmail.com>
2026-04-25 11:35:51 +0530

    Next up is probably a calc

---
+++ Basics/If-Elif-Else-Statement.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age: "))
+
+if age >= 18 and age < 100 :
+    print("Welcome to site")
+elif age < 18 and age > 0: 
+    print("You are too young")
+else :
+    print("Invalid input")   
