ed6ad10e728fdf57a56ebc5b0e38f8d0bc014a5d
Atharv-prx <atharvvrm6969@gmail.com>
2026-04-25 12:19:53 +0530

    Similar to ternary in c++

---
+++ Basics/Conditional-statement.py
@@ -0,0 +1,5 @@
+#Basically 1 line for if else
+
+num = float(input ("Enter a number to check if it is positive or negative: "))
+print ("Number is posittive" if num>0 else "Number is negative")
+print ("Number is even" if num % 2 == 0 else "Number is odd")
