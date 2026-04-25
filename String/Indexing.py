6a521c496594f294fba394bab1c6daba20573547
Atharv-prx <atharvvrm6969@gmail.com>
2026-04-25 15:00:42 +0530

    Can use this to reverse string

---
+++ String/Indexing.py
@@ -0,0 +1,11 @@
+# Indexing = accessing elements of a sequence using [] (indexing operator) 
+# [Start : end : step]
+
+credit_num = "1234-5678-9012-3456"
+
+print(credit_num[0])
+print(credit_num[0:4])
+print(credit_num[-1])
+print(credit_num[::2])  #Prints every 2nd character of string
+
+print(credit_num[::-1])   #Prints reverse string
