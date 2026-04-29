046cee529e962f9dbc7c9e33c507ee0bc339b1ab
Atharv-prx <atharvvrm6969@gmail.com>
2026-04-29 14:46:06 +0530

    Order of arguments matter

---
+++ Functions/User-defined.py
@@ -0,0 +1,9 @@
+#Function = Block of reusable code
+#           place () after the function name to invoke it
+
+def HappyBday(name, age):
+    print("Happy Bday to you!")
+    print(f"Happy Bday {name}")
+    print(f"You are {age} years old now!")
+
+HappyBday("Bro", 18)
