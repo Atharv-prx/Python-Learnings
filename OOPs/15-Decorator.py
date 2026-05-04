ada44af5e7111c496ad595f7747bf646917db730
Atharv-prx <atharvvrm6969@gmail.com>
2026-05-04 21:23:36 +0530

    Added some more conceptual clarity

--- OOPs/15-Decorator.py
+++ OOPs/15-Decorator.py
@@ -6,15 +6,15 @@ def add_sprinkles(func):
     # We need wrapper function so that this function is only called when we call get_icecream function
     def wrapper(*args, **kwargs): # We need args and kwargs here so that it can accept any king of arguments and keyword-arguments if needed
         print("You add sprinkles")
-        func(*args, **kwargs) # Same explanation as line 7
+        return func(*args, **kwargs) # Same explanation as line 7
     return wrapper
 
 #Decorator-2
 def add_fudge(func):
     def wrapper(*args, **kwargs):
         print("You add fudge")
-        func(*args, **kwargs)
-    return wrapper
+        return func(*args, **kwargs)
+    return wrapper # Return the function itself, if we use return wrapper()--> this would return result of execution 
 
 @add_sprinkles
 @add_fudge
