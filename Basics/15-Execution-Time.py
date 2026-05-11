3ea210962c50820f09d83290142613a914af1ccc
Atharv-prx <atharvvrm6969@gmail.com>
2026-05-11 23:17:33 +0530

    We can use sleep command to run command after said time

---
+++ Basics/15-Execution-Time.py
@@ -0,0 +1,11 @@
+import time
+
+start_time = time.perf_counter()
+
+# Main CODE GOES HERE
+
+end_time = time.perf_counter()
+
+elapsed_time = end_time - start_time
+
+print(f"Elapsed time: {elapsed_time:.2f} seconds")
