21324cadd4985fb4d8a80dc104e5d11f8507af54
Atharv-prx <atharvvrm6969@gmail.com>
2026-05-27 15:17:44 +0530

    Geometry to change width, length

---
+++ Tkinter/01-Windows.py
@@ -0,0 +1,10 @@
+from tkinter import *
+
+# widgets = GUI elements: buttons, textboxes, labels, images
+# windows = serves as a container to hold or contain these widge    
+
+window = Tk() # instantiate an instance of windows
+
+window.geometry("420x420")
+window.title("First GUI")
+window.mainloop() # Place window on computer screen, listen fo events
