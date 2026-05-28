93c9e489870c0424a5a81eaec253db5df7153525
Atharv-prx <atharvvrm6969@gmail.com>
2026-05-28 23:35:15 +0530

    Completed scale

--- Tkinter/07-Scale.py
+++ Tkinter/07-Scale.py
@@ -1,11 +1,24 @@
 from tkinter import *
+import os
 
 def submit():
     print("The tempt is : " + str(celsius_scale.get()) + " degree C") # celsius_scale.get() returns integer so we convert it to srting
 
 window = Tk()
 
-celsius_scale = Scale(window, from_=100, to=0)
+celsius_scale = Scale(window, 
+                      from_=100, 
+                      to=0,
+                      length= 500,
+                      orient=VERTICAL,
+                      font= ('Consolas',20),
+                      tickinterval=10, # adds numeric indicators for value
+#                     showvalue=0, # hide current value besides slider
+                      troughcolor= '#69EAFF',
+                      fg = '#FF1C00',
+                      bg = '#111111'
+                      )
+celsius_scale.set(((celsius_scale['from']-celsius_scale['to'])/2) +celsius_scale['to'])
 celsius_scale.pack()
 
 button = Button(window, text='submit', command=submit)
