9acbe924f688917556ec65a8f666aba31df5cd8b
Atharv-prx <atharvvrm6969@gmail.com>
2026-05-29 12:29:18 +0530

    Can't select more that one item at a time, so gonna add that next

--- Tkinter/08-Listboxes.py
+++ Tkinter/08-Listboxes.py
@@ -2,6 +2,18 @@
 
 from tkinter import *
 
+def submit():
+    print("You have ordered: ")
+    print(list_box.get(list_box.curselection()))
+
+def add():
+    list_box.insert(list_box.size(), entry_box.get())     # list_box.size()--> gives current position of index we are currently on
+    list_box.config(height=list_box.size())
+
+def delete():
+    list_box.delete(list_box.curselection())
+    list_box.config(height=list_box.size())
+    
 window = Tk()
 
 list_box = Listbox(window,
@@ -19,5 +31,17 @@ list_box.insert(4,"soup")
 list_box.insert(5,"salad")
 
 list_box.config(height=list_box.size()) # Adjusts size dynamically
+
+entry_box = Entry(window)
+entry_box.pack()
+
+submit_button = Button(window, text='Submit', command=submit )
+submit_button.pack()
+
+add_button = Button(window, text='Add', command=add)
+add_button.pack()
+
+delete_button = Button(window, text='Delete', command=delete)
+delete_button.pack()
 
 window.mainloop()
