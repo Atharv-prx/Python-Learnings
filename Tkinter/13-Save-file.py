84a0d0c8a12166350a0206236322ed7a9beae618
Atharv-prx <atharvvrm6969@gmail.com>
2026-05-30 16:14:46 +0530

    file = filedialog.asksaveasfilename(...) returns a string containing a file path, and string don't have .write() method so we can't use .write here

---
+++ Tkinter/13-Save-file.py
@@ -0,0 +1,27 @@
+from tkinter import *
+from tkinter import filedialog
+
+def saveFile():
+    file_path = filedialog.asksaveasfilename(
+        initialdir="C:\\Code\\Python\\Python-Learnings\\Tkinter", # place where we wanna save file goes here
+        defaultextension=".txt", 
+        filetypes=[     # supported extensions
+            ("Text File", "*.txt"),
+            ("HTML File", "*.html"),
+            ("All Files", "*.*")
+        ]
+    )
+
+    if file_path:  # User didn't cancel
+        with open(file_path, "w") as file:
+            file.write(text.get("1.0", END))
+
+window = Tk()
+
+button = Button(window, text='save', command=saveFile)
+button.pack()
+
+text = Text()
+text.pack()
+
+window.mainloop()
