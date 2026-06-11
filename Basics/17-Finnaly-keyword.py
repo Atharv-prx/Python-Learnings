b8323742e8f684145abcadd0a1d5f06b75680a87
Yash <atharvvrm6969@gmail.com>
2026-06-11 20:51:10 +0530

    some it is asked question on the finally keyword

---
+++ Basics/17-Finnaly-keyword.py
@@ -0,0 +1,19 @@
+def fun():
+    try:
+        l = [1,5,6,9]
+        i = int(input("Enter the index: "))
+        print(l[i])
+        return 1
+
+    except:
+        print("Some error occured")
+        return 0
+
+    finally:
+        print("Code in finally is always executed")
+
+x = fun()
+print(x)
+
+# finnaly is always exected 
+# I we use normally something instead of finnaly then it won't be executed cuz of "return" but finally is always executed
