cfaf4e790b7fdb343ecc7071e0cd25fd7465b969
Yash <atharvvrm6969@gmail.com>
2026-06-20 13:03:46 +0530

    Next up Labels

--- PyQt5/01-Window.py
+++ PyQt5/01-Window.py
@@ -1,9 +1,17 @@
 import sys 
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QIcon
+import os
+
+current_dir = os.path.dirname(__file__)
+image_path_1 = os.path.join(current_dir,"Images", "Legion Logo.jpg")
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle("First GUI lowkey, well ackshauly no")
+        self.setGeometry(0, 0, 900, 900) # (x,y,width,height) x,y at 0 makes window appear at upper left corner of screen
+        self.setWindowIcon(QIcon(image_path_1))
 
 def main():
     app = QApplication(sys.argv) # by passing sys.argv, it allows pyqt to proccess any command line arguments intended for it
